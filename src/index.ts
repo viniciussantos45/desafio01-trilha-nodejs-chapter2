c834cdff95d174f65a623e52a3fde42b263eb540
Vinicius Gomes <vinigomes.0311@gmail.com>
2022-03-23 20:18:44 -0300

    Documentation with Swagger

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
 import express from "express";
+import swagger from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerSetup from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", swagger.serve, swagger.setup(swaggerSetup));
 app.use((error, req, res, next) => {
   return res.json({ error });
 });
